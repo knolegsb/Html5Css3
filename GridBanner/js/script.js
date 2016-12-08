1fe9f9dd82d55c4f8d8a7d236c59bdd1d645a2ca
Sean <seon2you@hotmail.com>
2016-12-08 12:57:01 -0800

    Javascript

--- GridBanner/js/script.js
+++ GridBanner/js/script.js
@@ -1,1 +1,31 @@
-﻿
+﻿$(document).ready(function () {
+    var $items = $('.content > li:not(.center_content)');
+
+    //$items.animate({ "tempRotateY": 180 }, {
+    //    duration: 1000, easing: "easeOutQuad", step: function (now) {
+    //        $(this).css('transform', "rotateY(" + now + "deg)");
+    //    }
+    //});
+
+    $.each($items, function (index, target) {
+        $(this).delay(index * 150).prop({ "tempRotateY": 180 }).animate({ "tempRotateY": 90 }, {
+            duration: 500, easing: "easeInQuad", step: function (y) {
+                $(this).css('transform', "perspective(700) rotateY(" + y + "deg)");
+            },
+    //    })
+    //})
+    //$items.prop({ "tempRotateY": 180 }).animate({ "tempRotateY": 90 }, {
+    //    duration: 500, easing: "easeInQuad", step: function (y) {
+    //        $(this).css('transform', "rotateY(" + y + "deg)");
+    //    },
+
+        complete: function () {
+            $(this).find('.image').removeClass('none');
+            $(this).animate({ "tempRotateY": 0 }, {
+                duration: 500, easing: "easeOutQuad", step: function (y) {
+                    $(this).css('transform', "rotateY(" + y + "deg)");
+                }
+            });
+        
+    });
+});
