3d4578c582e6e4c57ff9d9cf15c34eab26f5ca73
Sean <seon2you@hotmail.com>
2016-12-08 16:59:55 -0800

    functions

--- GridBanner/js/script.js
+++ GridBanner/js/script.js
@@ -1,67 +1,92 @@
 ﻿
 $(document).ready(function () {
-    var $items = $('.content > li:not(.center_content)');
-   
-    $.each($items, function (index, target) {       
-        $(this).delay(index * 150).prop({ "tempRotateY": 180 }).animate({ "tempRotateY": 90 }, {
-            duration: 500, easing: "easeInQuad",
-            step: function (y) {
-                $(this).css('transform', "perspective(700) rotateY(" + y + "deg)");
-            },
+    var $images = $('.content img'),
+        count = $images.length;
 
-            complete: function () {               
-                $(this).find('.image').removeClass('none');
-
-                $(this).animate({ "tempRotateY": 0 }, {
-                    duration: 500, easing: "easeOutQuad",
-                    step: function (y) {
-                        $(this).css('transform', "perspective(700) rotateY(" + y + "deg)");
-                    }
-                });
-            }
-        });
-    });
-
-    $('.content').on('mouseenter', 'li:not(.center_content)', function () {
-        $(this).children().stop().animate({ left: '0px' }, 500);
-    });
-
-    $('.content').on('mouseleave', 'li:not(.center_content)', function () {
-        $(this).children().stop().animate({ left: '-200px' }, 500);
-    });
-
-    /**
-     * slide banner 
-     **/
-
-    var $contain = $('.center_content ul'),
-        loopList = $contain.children('li'),
-        currentIndex = 0,
-        totalIndex = loopList.length,
-        next_top = $contain.height(),
-        prev_top = next_top * -1;
-
-    setInterval(function () {
-        var $currentItem,
-            $nextItem,
-            nextIndex = currentIndex + 1;
-
-        if (nextIndex == totalIndex) {
-            nextIndex = 0;
+    $images.load(function () {
+        count--;
+        if (!count) {
+            executeContent();
         }
+    })
 
-        $currentItem = loopList.eq(currentIndex);
-        $nextItem = loopList.eq(nextIndex);
+    function executeContent() {
+        var $items = $('.content > li:not(.center_content)');
 
-        $nextItem.removeClass('none');
-        $nextItem.css('top', next_top);
+        var aniCount = $items.length;
 
-        $currentItem.animate({ top: prev_top }, 500, 'easeOutQuad', function () {
-            $(this).addClass('none');
+        $.each($items, function (index, target) {
+            $(this).delay(index * 150).prop({ "tempRotateY": 180 }).animate({ "tempRotateY": 90 }, {
+                duration: 500, easing: "easeInQuad",
+                step: function (y) {
+                    $(this).css('transform', "perspective(700) rotateY(" + y + "deg)");
+                },
+
+                complete: function () {
+                    $(this).find('.image').removeClass('none');
+
+                    $(this).animate({ "tempRotateY": 0 }, {
+                        duration: 500, easing: "easeOutQuad",
+                        step: function (y) {
+                            $(this).css('transform', "perspective(700) rotateY(" + y + "deg)");
+                        },
+                        complete: function () {
+                            aniCount--;
+                            if (!aniCount) {
+                                addEvent();
+                            }
+                        }
+                    });
+                }
+            });
         });
 
-        $nextItem.animate({ top: '0px' }, 500, 'easeOutQuad');
-        currentIndex = nextIndex;
-    }, 1500);
+        function addEvent() {
+            $('.content').on('mouseenter', 'li:not(.center_content)', function () {
+                $(this).children().stop().animate({ left: '0px' }, 500);
+            });
+
+            $('.content').on('mouseleave', 'li:not(.center_content)', function () {
+                $(this).children().stop().animate({ left: '-200px' }, 500);
+            });
+        }        
+
+        /**
+         * slide banner 
+         **/
+
+        var $contain = $('.center_content ul'),
+            loopList = $contain.children('li'),
+            currentIndex = 0,
+            totalIndex = loopList.length,
+            next_top = $contain.height(),
+            prev_top = next_top * -1;
+
+        $contain.removeClass('none').css('opacity', 0);
+        $contain.delay(1500).animate({ 'opacity': 1 }, 500, function () {
+            setInterval(function () {
+                var $currentItem,
+                    $nextItem,
+                    nextIndex = currentIndex + 1;
+
+                if (nextIndex == totalIndex) {
+                    nextIndex = 0;
+                }
+
+                $currentItem = loopList.eq(currentIndex);
+                $nextItem = loopList.eq(nextIndex);
+
+                $nextItem.removeClass('none');
+                $nextItem.css('top', next_top);
+
+                $currentItem.animate({ top: prev_top }, 500, 'easeOutQuad', function () {
+                    $(this).addClass('none');
+                });
+
+                $nextItem.animate({ top: '0px' }, 500, 'easeOutQuad');
+                currentIndex = nextIndex;
+            }, 1500);
+        });        
+    }
 });
 
